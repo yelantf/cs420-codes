9178a82a47acef3755af6ab4d57bbf333dceb645
yelantf <yelantingfeng@sjtu.edu.cn>
2018-06-15 00:52:24 +0800

    forest

--- traditional_methods/RandomForest/ForestBestN.py
+++ traditional_methods/RandomForest/ForestBestN.py
@@ -1,7 +1,5 @@
 import numpy as np
-from sklearn.svm import SVC,LinearSVC
-from sklearn.decomposition import PCA
-from sklearn.multiclass import OneVsOneClassifier
+from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
 import seaborn as sns
 from util import *
@@ -15,16 +13,12 @@ test_label=np.fromfile("../../mnist/mnist_test/mnist_test_label",dtype=np.uint8)
 
 accuList=[]
 runtimeList=[]
-# trying different dimension numbers from 5 to 250
-for i in xrange(5,251,5):
-    # Dimensionality reduction to target dimension number
-    pca=PCA(n_components=i)
-    train_data_reduce=pca.fit_transform(train_data)
-    test_data_reduce=pca.transform(test_data)
-    print 'dim:{}'.format(i),
-    # Using LinearSVC on i-dimensional data
-    clf=OneVsOneClassifier(LinearSVC(),n_jobs=20)
-    train_time,test_time,accu=run_model(clf,train_data_reduce,train_label,test_data_reduce,test_label)
+# trying different decision tree numbers from 5 to 250
+for n_model in xrange(5,251,5):
+    print 'n_model:{}'.format(n_model),
+    # Using random forest with n_model decision trees.
+    clf=RandomForestClassifier(n_estimators=n_model,n_jobs=10)
+    train_time,test_time,accu=run_model(clf,train_data,train_label,test_data,test_label)
     # print the performance
     print 'train time:{} test time:{} accuracy:{}'.format(train_time,test_time,accu)
     runtimeList.append(train_time+test_time)
@@ -33,13 +27,13 @@ for i in xrange(5,251,5):
 # Plot the accuracy figure.
 plt.figure()
 plt.plot(np.arange(5,251,5),accuList)
-plt.xlabel('dimension')
+plt.xlabel('the number of trees')
 plt.ylabel('accuracy')
-plt.savefig('pca-accu.pdf') 
+plt.savefig('forest-accu.pdf')
 
 # Plot the runtime figure.
 plt.figure()
 plt.plot(np.arange(5,251,5),runtimeList,color='coral')
-plt.xlabel('dimension')
+plt.xlabel('the number of trees')
 plt.ylabel('runtime')
-plt.savefig('pca-runtime.pdf')
+plt.savefig('forest-runtime.pdf')
